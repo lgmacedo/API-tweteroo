8151900291d839d31dfadb40a3069bc326adb09b
lgmacedo <lucasguerra.macedo@gmail.com>
2023-04-05 13:50:06 -0300

    GET tweets now returns only 10 last tweets, from newest to oldest

--- src/app.js
+++ src/app.js
@@ -16,27 +16,34 @@ const tweets = [
   },
 ];
 
-function getAvatar(username){
-    for(let i=0; i<users.length; i++){
-        if (users[i].username === username){
-            return users[i].avatar;
-        }
+function getAvatar(username) {
+  for (let i = 0; i < users.length; i++) {
+    if (users[i].username === username) {
+      return users[i].avatar;
     }
-    return null;
+  }
+  return null;
+}
+
+function get10LastTweets(tweets) {
+  const arrTweets = [];
+  for (let i = tweets.length - 1; i >= 0; i--) {
+    arrTweets.push({
+      username: tweets[i].username,
+      avatar: getAvatar(tweets[i].username),
+      tweet: tweets[i].tweet,
+    });
+    if (arrTweets.length === 10) break;
+  }
+  return arrTweets;
 }
 
 const app = express();
 app.use(cors());
 
 app.get("/tweets", (req, res) => {
-  res.send(
-    tweets.map((t) => ({
-      username: t.username,
-      avatar:
-        getAvatar(t.username),
-      tweet: t.tweet,
-    }))
-  );
+  res.send(get10LastTweets(tweets));
 });
 
-app.listen(5000, () => console.log("Running server on port 5000"));
+const PORT = 5000;
+app.listen(PORT, () => console.log(`Running server on port ${PORT}`));
